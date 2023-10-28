55c6dd4eb16ffdd68133c2a6667a3eccc71d581e
João Moura Lima <jmoura.dev@gmail.com>
2023-10-28 00:12:52 -0300

    chore: add bybug gem and fix proponents factory bot

--- spec/factories/proponents.rb
+++ spec/factories/proponents.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :proponents do
+  factory :proponent do
     name { Faker::Name.name }
     cpf { Faker::IDNumber.brazilian_citizen_number(formatted: true) }
     birthdate { Faker::Time.between(from: 60.years.ago, to: 18.years.ago) }
