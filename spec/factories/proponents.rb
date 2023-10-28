55e3922b89f49ad1fc06c8e6ba625a256a7ca162
João Moura Lima <jmoura.dev@gmail.com>
2023-10-27 23:19:28 -0300

    chore: proponents tests

---
+++ spec/factories/proponents.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :proponents do
+    name { Faker::Name.name }
+    cpf { Faker::IDNumber.brazilian_citizen_number(formatted: true) }
+    birthdate { Faker::Time.between(from: 60.years.ago, to: 18.years.ago) }
+    wage { Faker::Number.decimal(l_digits: 4) }
+  end
+end
---
+++ spec/models/proponent_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Proponent, type: :model do
+  describe 'associations' do
+    it { should have_one(:address).class_name('Address') }
+    it { should have_many(:contact_phones).class_name('ContactPhone') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:cpf) }
+    it { should validate_presence_of(:birthdate) }
+    it { should validate_length_of(:cpf).is_equal_to(14) }
+  end
+end
