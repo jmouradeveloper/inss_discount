e15921c453b2bc9b1618f0cebccb0aecd575cdda
João Moura Lima <jmoura.dev@gmail.com>
2023-10-27 23:37:13 -0300

    chore: addresses tests

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :address do
+    street_name { Faker::Address.street_name }
+    number { Faker::Address.building_number }
+    neighborhood { Faker::Address.community }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    proponent
+  end
+end
---
+++ spec/models/address_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Address, type: :model do
+  describe 'associations' do
+    it { should belong_to(:proponent).class_name('Proponent') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:street_name) }
+    it { should validate_presence_of(:number) }
+    it { should validate_presence_of(:neighborhood) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+  end
+end
