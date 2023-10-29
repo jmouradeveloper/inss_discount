73fec2d5fad58a86e4b269ff0a033c8c792cd9a6
João Moura Lima <jmoura.dev@gmail.com>
2023-10-29 01:09:25 -0300

    chore: update proponent test

---
+++ spec/requests/proponents/update_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Edit Proponent", type: :request do
+  let!(:proponent) { create(:proponent, name: 'Original Proponent') }
+
+  subject { put proponent_path(proponent), params: proponent_params }
+
+  context 'on success' do
+    let(:proponent_params) do
+      {
+        proponent: {
+          name: 'Edited Proponent',
+          cpf: '123.456.789-10',
+          birthdate: DateTime.parse("1993-05-10 10:30:14"),
+          wage: 12400.53
+        }
+      }
+    end
+
+    it do
+      expect { subject }.to change { Proponent.count }.by(0)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(proponent_path(Proponent.last))
+
+      proponent.reload
+      expect(proponent.name).to eq(proponent_params[:proponent][:name])
+      expect(proponent.cpf).to eq(proponent_params[:proponent][:cpf])
+      expect(proponent.birthdate).to eq(proponent_params[:proponent][:birthdate])
+      expect(proponent.wage).to eq(proponent_params[:proponent][:wage])
+    end
+  end
+end
