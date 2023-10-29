7f0bee6bcb70fe6d022d1fd818b86208db5a2888
João Moura Lima <jmoura.dev@gmail.com>
2023-10-29 01:09:12 -0300

    chore: create proponent test

--- spec/requests/proponents/create_spec.rb
+++ spec/requests/proponents/create_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe "Create Proponent", type: :request do
   subject { post proponents_path, params: proponent_params }
 
   context 'on success' do
-    let(:proponent_params) { attributes_for(:proponent) }
+    let(:proponent_params) { { proponent: attributes_for(:proponent) } }
 
-    it { expect { subject }.to change { Proponent.count }.by(1) }
-    it { expect(response).to have_http_status(:created) }
+    it do
+      expect { subject }.to change { Proponent.count }.by(1)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(proponent_path(Proponent.last))
+    end
   end
 end
