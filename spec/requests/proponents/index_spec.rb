1b6b44758607e603cc7f433c556dd5504267d3bf
João Moura Lima <jmoura.dev@gmail.com>
2023-10-29 02:06:20 -0300

    chore: list proponent tests

---
+++ spec/requests/proponents/index_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "List Proponent", type: :request do
+  context 'on success' do
+    let(:default_per_page) { 5 }
+    let(:three_pages_of_proponents) { (default_per_page * 2) + 1 }
+
+    before do
+      create_list(:proponent, three_pages_of_proponents)
+      get proponents_path
+    end
+
+    it { should render_template('index') }
+    it { expect(response).to have_http_status(:ok) }
+    it { expect(assigns(:proponents).count).to eq(default_per_page) }
+  end
+end
