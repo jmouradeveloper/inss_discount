719f825b54bf3857a7a99422453be507416be886
João Moura Lima <jmoura.dev@gmail.com>
2023-10-30 01:07:21 -0300

    chore: change inss calculate route tests

--- app/controllers/financial_controller.rb
+++ app/controllers/financial_controller.rb
@@ -2,7 +2,7 @@ class FinancialController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def calculate_inss
-    inss_discount = InssDiscountCalculator.call(params[:wage])
+    inss_discount = InssDiscountCalculator.call(wage: params[:wage])
 
     render json: { amount: inss_discount }, status: :ok
   end
--- spec/requests/financial/calculate_inss_spec.rb
+++ spec/requests/financial/calculate_inss_spec.rb
@@ -2,14 +2,17 @@ require "rails_helper"
 
 RSpec.describe "Calculate INSS", type: :request do
   context 'on success' do
-    let(:params) { { wage: 12_000 } }
+    let(:wage) { '3000' }
 
-    before { post financial_calculate_inss_path, params: params }
+    before do
+      expect(InssDiscountCalculator).to receive(:call).with(wage: wage).and_return(281.62)
+      post financial_calculate_inss_path, params: { wage: wage }
+    end
 
     it do
       expect(response).to have_http_status(200)
       response_json = JSON.parse(response.body)
-      expect(response_json['amount']).to eq(1200)
+      expect(response_json['amount']).to eq(281.62)
     end
   end
 end
