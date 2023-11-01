0f4f2acd31146ea09cb24768f6a164b38ee88fc6
João Moura Lima <jmoura.dev@gmail.com>
2023-11-01 02:05:40 -0300

    chore: create taxes constants

---
+++ lib/inss_taxes/constants.rb
@@ -0,0 +1,18 @@
+module InssTaxes
+  module Constants
+    TAX_BY_WAGE_RANGE = {
+      7.5 => {
+        wage_range: 0...1_045.00
+      },
+      9 => {
+        wage_range: 1_045.01...2_089.60
+      },
+      12 => {
+        wage_range: 2_089.61...3_134.40
+      },
+      14 => {
+        wage_range: 3_134.41...6_101.06
+      }
+    }
+  end
+end
