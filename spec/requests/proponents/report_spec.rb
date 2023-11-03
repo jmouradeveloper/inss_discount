bd0a3fa86beeb4116ead53275b398e0e7142381e
João Moura Lima <jmoura.dev@gmail.com>
2023-11-02 23:59:54 -0300

    chore: proponent report request test

---
+++ spec/requests/proponents/report_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Report Proponent List", type: :request do
+  context 'on success' do
+    before do
+      create_list(:proponent, 10)
+      get report_proponents_path
+    end
+
+    it { should render_template('report') }
+    it { expect(response).to have_http_status(:ok) }
+    it { expect(assigns(:proponents_by_wage_rage)).to be_truthy }
+  end
+end
