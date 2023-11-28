e2adc80d56a098e9a116d2e6e84f09d5ac9b330e
João Moura Lima <jmoura.dev@gmail.com>
2023-11-28 00:32:54 -0300

    feat: proponents grouper service

--- app/services/proponents/grouper.rb
+++ app/services/proponents/grouper.rb
@@ -1,32 +1,7 @@
 class Proponents::Grouper
   class << self
     def by_wage_range(proponents)
-        {
-          'key1': [
-            {
-              name: 'teste',
-              cpf: '054.264.888-90'
-            }, {
-              name: 'teste 2',
-              cpf: '054.264.888-91'
-            }
-          ],
-          'key2': [
-            {
-              name: 'teste',
-              cpf: '054.264.888-90'
-            }, {
-              name: 'teste 2',
-              cpf: '054.264.888-91'
-            }
-          ]
-        }
-    end
-
-    private
-
-    def private_method
-      
+      proponents.group_by { |proponent| Financial::TaxMatcher.call(wage: proponent.wage) }
     end
   end
 end
