5079711cde6fe5be2c4baddb09f41d3c8028b902
João Moura Lima <jmoura.dev@gmail.com>
2023-11-28 00:35:38 -0300

    refactor: extract class tax matcher

--- app/services/financial/inss_discount_calculator.rb
+++ app/services/financial/inss_discount_calculator.rb
@@ -2,38 +2,29 @@ class Financial::InssDiscountCalculator
   class << self
     def call(wage:)
       wage = wage.to_f
-      tax_match = tax_match(wage)
+      tax_match = Financial::TaxMatcher.call(wage)
 
       calculate_inss(wage, tax_match)
     end
 
     private
 
-    def tax_match(wage)
-      taxes_by_wage_range.map do |range|
-        break range.first if range.second[:wage_range].member?(wage)
-      end
-    end
-
     def calculate_inss(wage, tax_match)
-      inss_value = taxes_by_wage_range.reduce(0) do |amount, (tax, value)|
-        actual_max_value = value[:wage_range].last
-        actual_min_value = value[:wage_range].first
-        previous_max_value = last_iteration_max_value(actual_min_value)
-        tax_percentage = tax_percentage(tax)
+      inss_value = InssTaxes::Constants::TAX_BY_WAGE_RANGE
+        .reduce(0) do |amount, (tax, value)|
+          actual_max_value = value[:wage_range].last
+          actual_min_value = value[:wage_range].first
+          previous_max_value = last_iteration_max_value(actual_min_value)
+          tax_percentage = tax_percentage(tax)
 
-        if tax_match == tax
-          break calculation_result(wage, previous_max_value, tax_percentage) + amount 
+          if tax_match == tax
+            break calculation_result(wage, previous_max_value, tax_percentage) + amount 
+          end
+
+          calculation_result(actual_max_value, previous_max_value, tax_percentage) + amount
         end
 
-        calculation_result(actual_max_value, previous_max_value, tax_percentage) + amount
-      end
-
       inss_value.ceil(2)
-    end
-
-    def taxes_by_wage_range
-      @taxes_by_wage_range ||= InssTaxes::Constants::TAX_BY_WAGE_RANGE
     end
 
     def last_iteration_max_value(actual_min_value)
---
+++ app/services/financial/tax_matcher.rb
@@ -0,0 +1,20 @@
+class Financial::TaxMatcher
+  class << self
+    def call(wage:)
+      taxes_by_wage_range.find do |tax, range|
+        tax if range[:wage_range].member?(wage) || last_range?(tax)
+      end
+        .first
+    end
+
+    private
+
+    def taxes_by_wage_range
+      @taxes_by_wage_range ||= InssTaxes::Constants::TAX_BY_WAGE_RANGE
+    end
+
+    def last_range?(tax)
+      tax == taxes_by_wage_range.keys.last
+    end
+  end
+end
