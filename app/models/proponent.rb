a023e39810a269a41e537bd81b7681188e4ff18a
João Moura Lima <jmoura.dev@gmail.com>
2023-12-01 01:42:41 -0300

    style: rubicop suggestions

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Proponent < ApplicationRecord
-  has_one :address
-  has_many :contact_phones
+  has_one :address, dependent: :destroy
+  has_many :contact_phones, dependent: :destroy
 
   validates :name, :cpf, :birthdate, presence: true
   validates :cpf, length: { is: 14 }
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,7 +26,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join('spec/fixtures')
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
